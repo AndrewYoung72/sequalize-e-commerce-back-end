85f2b362723f6dbe5213cea2dd76b941d034dda7
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-05-18 09:29:35 -0600

    product route work

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,14 +7,16 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findAll();
+    const categoryData = await Category.findAll({
+      include: [{model:Product}],
+    });
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
@@ -22,7 +24,7 @@ router.get('/:id', (req, res) => {
       include: [{ model: Product, through: Category, as: 'Associated products by category.'}]
     });
     if (!categoryData) {
-      res.status(404).json({ message: 'No product found with this id!' });
+      res.status(404).json({ message: 'No category found with this id!' });
       return;
     }
     res.status(200).json(categoryData);
